bc62530e27e3bd25379c4c259a8f668945e7f9ee
kevin <kevin@squirrel.fr>
2021-08-08 22:44:36 +0400

    first commit

---
+++ components/Footer.js
@@ -0,0 +1,43 @@
+function Footer() {
+    return (
+        <div className="grid grid-cols-1 md:grid-cols-4 gap-y-10 px-32 py-14 bg-gray-100 text-gray-600">
+            <div className="space-y-4 text-xs text-gray-800">
+                <h5 className="font-bold">ABOUT</h5>
+                <p>How airbnb works</p>
+                <p>Newsroom</p>
+                <p>Investors</p>
+                <p>Airbnb Plus</p>
+                <p>Airbnb Luxe</p>
+            </div>
+
+            <div className="space-y-4 text-xs text-gray-800">
+                <h5 className="font-bold">COMMUNITY</h5>
+                <p>Accessibility</p>
+                <p>This is not a real site</p>
+                <p>It's a pretty awesome clone</p>
+                <p>Referal Accepted</p>
+                <p>Kevin GR</p>
+            </div>
+
+            <div className="space-y-4 text-xs text-gray-800">
+                <h5 className="font-bold">HOST</h5>
+                <p>Accessibility</p>
+                <p>This is not a real site</p>
+                <p>It's a pretty awesome clone</p>
+                <p>Referal Accepted</p>
+                <p>Kevin GR</p>
+            </div>
+
+            <div className="space-y-4 text-xs text-gray-800">
+                <h5 className="font-bold">Support</h5>
+                <p>Accessibility</p>
+                <p>This is not a real site</p>
+                <p>It's a pretty awesome clone</p>
+                <p>Referal Accepted</p>
+                <p>Kevin GR</p>
+            </div>
+        </div>
+    )
+}
+
+export default Footer
